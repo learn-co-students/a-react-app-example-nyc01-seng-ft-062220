b64a8ff36fbb8da21f24221a94690fefae2dfd1c
DCR430 <daniel.c.reyes30@gmail.com>
2020-08-23 17:28:36 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -11,14 +11,17 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {moment().format('MMMM Do YYYY, hh:mm:ss a')}
+          "Now"
+          {/* {moment().format('MMMM Do YYYY, hh:mm:ss a')} */}
         </header>
         <p className="App-intro">
-          In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax.
+          In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax. 
           JSX lets us include JavaScript functions right along with the HTML, and also
-          allows us to add in components, which are separate, self-contained chunks of JSX.
+           allows us to add in components, which are separate, self-contained chunks of JSX.
+          {/* DOLLY STAY ON THE PHONE */}
         </p>
         <ExampleComponent />
+        <TestComponent />
 
       </div>
     );
